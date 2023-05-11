027bd0a8c59439d49aff7f157ca682085e53d150
Ricardo Ganter <ricardorosalesganter@gmail.com>
2023-05-10 21:58:39 -0400

    cambio total de las opciones (laterales como market, home...) con nuevos estilos muy facheros

--- components/navbar/opcion.jsx
+++ components/navbar/opcion.jsx
@@ -1,18 +1,30 @@
 import Link from 'next/link';
 import styles from '../../src/styles/navbar/opcion.module.scss'
-
+import Image from 'next/image';
+import market from '../../public/icon/shop-solid.svg'
+import home from '../../public/icon/house-solid.svg'
+import scolarship from '../../public/icon/file-signature-solid.svg'
+import news from '../../public/icon/bullhorn-solid.svg'
 
 const Opcion = ()=>{
     return(
         <>
             <div className={styles.contain}>
-                <Link href={'/'}> <button> Home </button>  </Link>
-                <Link href={'/noticias'}> <button> News </button> </Link>
-                <button>ScolarShips</button>
-                <Link href={'/market'}> <button>Market</button></Link>
-                
-            </div>
-            <div className={styles.containrigth}>
+                <div>
+                    <Link className={styles.option} href={'/'}>
+                    <Image src={home} width={30} style={{margin: " 0 1rem"}}/>
+                        <p> Home </p>  </Link>
+                    <Link href={'/noticias'} className={styles.option}> 
+                    <Image src={news} width={30} style={{margin: " 0 1rem"}}/>
+                        <p> News </p> </Link>
+                    <Link href={'/cambiame'} className={styles.option}>
+                    <Image src={scolarship} width={30} style={{margin: " 0 1rem"}}/> 
+                        <p>ScolarShips</p>
+                        </Link>
+                    <Link href={'/market'}className={styles.option}> 
+                    <Image src={market} width={30} style={{margin: " 0 1rem"}}/> 
+                        <p>Market</p></Link>
+                </div>
             </div>
         </>
     )
