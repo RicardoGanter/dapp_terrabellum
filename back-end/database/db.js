be498bd0a868912e1b8bd0b30fab38dcc5b74950
RicardoGanter <ricardorosalesganter@gmail.com>
2023-05-11 19:17:20 -0400

    lol

--- back-end/database/db.js
+++
@@ -1,7 +0,0 @@
-import {Sequelize} from 'sequelize';
-const db = new Sequelize('Sas','admin','terrabellum23',{
-    host:'prueba.cccsrfisiszh.sa-east-1.rds.amazonaws.com',
-    dialect:'mysql',
-    port:'3306'
-});
-export default db;
