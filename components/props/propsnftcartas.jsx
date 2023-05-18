6113bcb784cafbf92f3d4b9ce430e825ef2e4ba2
RicardoGanter <ricardorosalesganter@gmail.com>
2023-05-18 02:38:58 -0400

    ruta fusion visualmente casi completa, falta agregar todas las funcionalidadews

--- components/props/propsnftcartas.jsx
+++ components/props/propsnftcartas.jsx
@@ -1,18 +1,34 @@
 import styles from "../../src/styles/props/propsnftcartas.module.scss";
 import Link from "next/link";
-const PropsNftcartas = ({ Href,name,img,Ida,Rare, Level}) => {
+import { useEffect, useState } from "react";
+const PropsNftcartas = ({ Href,name,img,Ida,Rare, Level, height }) => {
+  
+  const width = height * 0.7
+  const [size_pre , setsize_pre] = useState();
+  const [fontsize, setFontsize] = useState();
+  useEffect(() => {
+    const size = height/ 25 + "px"
+    setFontsize(size)
+      const tonumber = Number(size.replace("px", ""))
+      const a =   tonumber * 3 + 40
+      if ( height > 40 ){
+        const lal = height / 40
+        setsize_pre( a + lal)
+      }
+      //  400 = 50   800 = 60   200 = 45  100 = 42.5 0 = 40 
+  },[])
+   
 
   return (
-    <Link className={styles.link} href={`/market/${Href}`}>
+    <Link className={styles.link} href={`/market/${Href}`  }>
     { Rare==="normal" ? (
-      <div className={styles.cards}>
+      <div className={styles.cards} style={{width:width,height:height}}>
       <div className={styles.contain}>
-      <img src={img}  className={styles.nft}>
-      </img>
+      <img src={img}  className={styles.nft}  height={height - size_pre}/>
       <div className={styles.hability}>
-        <pre>Name  : {name} </pre>
-        <pre>Rarity: {Rare}</pre>
-        <pre>Level : {Level} </pre>
+        <pre style={{fontSize: fontsize }} >Name  : {name} </pre>
+        <pre style={{fontSize: fontsize }} >Rarity: {Rare}</pre>
+        <pre style={{fontSize: fontsize }} >Level : {Level} </pre>
       </div>
       {/* <h1>{Ida}</h1> */}
       </div>
--- src/app/user/fusion/page.jsx
+++ src/app/user/fusion/page.jsx
@@ -1,80 +1,113 @@
 "use client"
-import styles from "../../../styles/user/inventario/infocarta.module.scss";
-import PropsNftcartas from "../../../../components/props/propsnftcartas";
+import styles from "../../../styles/user/fusion.module.scss";
 import img from '../../../../public/img/lal.webp'
 import Image from "next/image";
 import { ethers } from "ethers";
-// import connectWallet from "@/etherjs/borrador";
-// import connectWallet,{web3Modal} from "@/etherjs/borrador";
 import { useEffect,useState } from "react";
-// import getAddress from "./getaddress";
-// import getNFTs from "./idnft";
+import PropsNftcartas from "../../../../components/props/propsnftcartas";
+
 const Fusion = () => {
-    const [fusion, setFusion]= useState(false)
-    const [confirmfusion, setConfirmfusion]= useState(true)
-    const [confirmdefusion, setConfirmdefusion]= useState(false)
-    const switchfusion = ()=>{
-      if(confirmfusion===true){
-        setConfirmfusion(!confirmfusion)
-      }
-    }
-    const switchdefusion = ()=>{
-      if(confirmdefusion===true){
-        setConfirmdefusion(!confirmdefusion)
-      }
-    }
+    
   return (
     <>
         <div className={styles.contain}>
-          <div className={styles.info}>
-            <div className={styles.infoleft}>
-                <PropsNftcartas/>
-                <div className={styles.sell}>
-                    <div>Vender </div>
-                    <div>Subastar</div>
+            {/* TOP */}
+            <div>
+            <div className={styles.containnfts}>
+              {/* FILTERS */}
+              <div className={styles.filter}>
+                <div className={styles.filter1}>
+                  <div className={styles.filterinput}>
+                    <p>Characters</p>
+                    <input type="text" />
+                  </div>
+                  <div className={styles.filterinput}>
+                    <p>Skills</p>
+                    <input type="text" />
+                  </div>
+                 <div className={styles.filterinput}>
+                  <p>Rarity</p>
+                  <input type="text" /> 
+                 </div>
+                  <div className={styles.filterinput}> 
+                    <p> Level </p>
+                    <div>
+                      <p>1</p>
+                    </div>
+                    <div>
+                      <p>2</p>
+                    </div>
+                    <div>
+                      <p>3</p>
+                    </div>
+                    {/* <input type="checkbox" name="" id="" />
+                    <input type="checkbox" name="" id="" />
+                    <input type="checkbox" name="" id="" />  */}
+                  </div>
                 </div>
-                <div className={styles.fusion} onClick={()=>{setFusion(!fusion)}} >Fusion/desfusion</div>
-            </div>
-            {/* --------------------------------------- */}
-            <div className={styles.inforight}>
-              <div className={styles.containhabilidades}>
-                <Image src={img} alt='lol'/>
-                <Image src={img} alt='lol'/>
-                <Image src={img} alt='lol'/>
               </div>
-              <div className={styles.infohabilidades}>
-               <div>asdasdasdasdasdadasd mamamam huevo fluglguglguggluggklgu</div>
+              {/* FILTERS */}
+
+              {/* NFTS */}
+              <div className={styles.nfts}>
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+                <PropsNftcartas Rare="normal" height={270} />
+              </div>
+              {/* NFTS */}
+            </div>
+
+            <div className={styles.fusionnft}>
+              <div className={styles.containfusionnft}>
+                <PropsNftcartas Rare="normal" height={250} />
+                <PropsNftcartas Rare="normal" height={250} />
+                <PropsNftcartas Rare="normal" height={250} />
               </div>
             </div>
-          </div>
-          {/* --------------------FUSION-------------------- */}
-          { fusion ? 
-          <div className={styles.containfusion}>
-            <div className={styles.nftfusion}>aaaa</div>
-            <div className={styles.cartsfusion}>
-                <PropsNftcartas/>
-                <PropsNftcartas/>
-                <PropsNftcartas/>
             </div>
-            <div className={styles.btndefusion}> 
-                <div onClick={()=>{setConfirmfusion(!confirmfusion),switchdefusion()}}>fusion</div>
-                <div onClick={()=>{setConfirmdefusion(!confirmdefusion),switchfusion()}}>defusion</div>
-            </div>
-              { confirmfusion ? 
+            {/* TOP */}
+
+
+              {/* BOTTOM */}
+            <div className={styles.containtypemerge}>
+              {/* TYPE OF MERGE ?  */}
+              <div className={styles.top}>
+
+              <div className={styles.left}>
+                <PropsNftcartas Rare="normal" height={320}/>
+                <h1 className={styles.fusion}>merge</h1>
+              </div>
               
-              <div onClick={()=>{connectWallet()}} className={styles.btnaceptfusion}>confirmar fusion</div>  
-              :null
-            }
-            { confirmdefusion ?
+              <div className={styles.right}>
+                <div><h1>hability1</h1> <h1>hability2</h1> <h1>hability3</h1></div>                
+                <h1>unmerge</h1>
+              </div>
 
-              <div onClick={()=>{connectWallet()}} className={styles.btnaceptfusion}>confirmar defusion</div>  
-              :null
-
-            }
-          </div> : null}
-          
+              </div>
+                {/* FUNCION FUSION */}
+              <div className={styles.bottom}>
+                  <PropsNftcartas Rare="normal" height={300} />
+                  
+                  <div className={styles.containcircle}> 
+                    <div style={{display: "flex", alignItems:"center", gap:"1rem" }}>
+                      <div className={styles.circleG}/> 
+                      <h3>66%</h3>
+                  </div> 
+                  <div style={{display: "flex", alignItems:"center", gap:"1rem" }}>
+                     <div className={styles.circleM}/>
+                     <h3>33%</h3>
+                  </div>
+                  <button>Merge</button>
+                  </div>
+              </div>
+            </div>
+              {/* BOTTOM */}
         </div>
-        <PropsNftcartas/>
     </>
   );
 };
