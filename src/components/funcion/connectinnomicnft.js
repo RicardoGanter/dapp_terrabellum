347603e9504cfab8170744a2120c64e222b569ac
RicardoGanter <ricardorosalesganter@gmail.com>
2023-07-03 19:12:07 -0400

    test proyect 0 errors

--- src/components/funcion/connectinnomicnft.js
+++ src/components/funcion/connectinnomicnft.js
@@ -2,7 +2,7 @@ import NetworkGoerliEth from "./network.js";
 import { ethers } from "ethers";
 const ConnectInnomicNft = async () =>{
     const signer = await NetworkGoerliEth();
-    const abi = require("../../blockchain/abi/abi.js");
+    const abi = require("../../components/abi/abi.js");
     const contractAddress = "0x436Af34eBF3085cE92780c853e429d29A2CE4AA1" ;  //CONTRATO PRINCIPAL (INNOMICNFT)
     const contract = new ethers.Contract(
         contractAddress,
